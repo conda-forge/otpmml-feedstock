3b244b8fd56fb7290a1153ea5d9d2ba52f20cfb2
Julien Schueller <schueller@phimeca.com>
2022-12-05 16:03:05 +0100

    Update build.sh

--- recipe/build.sh
+++ recipe/build.sh
@@ -5,8 +5,8 @@ mkdir build && cd build
 cmake \
   -DCMAKE_PREFIX_PATH=${PREFIX} \
   -DCMAKE_INSTALL_PREFIX=${PREFIX} \
-  -DBUILD_DOC=OFF \
+  -DBUILD_DOC=OFF -LAH \
   ..
 
-make install -j${CPU_COUNT}
+make install -j${CPU_COUNT} VERBOSE=1
 DYLD_FALLBACK_LIBRARY_PATH=${PREFIX}/lib ctest -R pyinstallcheck --output-on-failure -j${CPU_COUNT}
