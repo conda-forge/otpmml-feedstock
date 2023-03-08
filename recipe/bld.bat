7db64e35256e64f9de5ea394c93e96352ccf7b37
Julien Schueller <schueller@phimeca.com>
2023-03-08 11:51:42 +0100

    create symlink

--- recipe/bld.bat
+++ recipe/bld.bat
@@ -3,3 +3,8 @@ if errorlevel 1 exit 1
 
 %PKG_NAME%-%PKG_VERSION%-py%PY_VER%-x86_64.exe /userlevel=1 /S /FORCE /D=%PREFIX%
 if errorlevel 1 exit 1
+
+cd %SP_DIR%\otpmml
+mklink libotpmml2.dll libotpmml.dll
+if errorlevel 1 exit 1
+
